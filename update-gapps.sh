fd6953188a78d4847217a1e5c14eac383cafd5c4
cocide <cocide@evervolv.com>
2011-02-27 16:40:29 -0600

    Added the ability to make a GApps update zip using make. Also corrected problems with getting new Apps from a phone and the Makefile.
    *You can now pull updated GApps from a phone and then make a flashable zip*
    
    *usage*
    To update GApps from a phone - build/update-gapps.sh
    To make a flashable GApps zip - make gapps
    
    Change-Id: I67eb592e85aa5388d2466f67adb3ca1786996659

--- update-gapps.sh
+++ update-gapps.sh
@@ -2,7 +2,45 @@
 
 #script for evervolv written by cocide
 #will pull gapps related packages off the primary connected phone using adb and put the files into the gapps folder inside the repo (vendor/evervolv/GAPPS)
-echo "if you have premissions problems kill the adb server (adb kill-server) and start it again with sudo (sudo adb start-server)"
+echo "if you have permissions problems kill the adb server (adb kill-server) and start it again with sudo (sudo adb start-server)"
+
+rm -f vendor/evervolv/GAPPS/system/app/CarHomeGoogle.apk
+rm -f vendor/evervolv/GAPPS/system/app/CarHomeLauncher.apk
+rm -f vendor/evervolv/GAPPS/system/app/Facebook.apk
+rm -f vendor/evervolv/GAPPS/system/app/GenieWidget.apk
+rm -f vendor/evervolv/GAPPS/system/app/Gmail.apk
+rm -f vendor/evervolv/GAPPS/system/app/GoogleBackupTransport.apk
+rm -f vendor/evervolv/GAPPS/system/app/GoogleCalendarSyncAdapter.apk
+rm -f vendor/evervolv/GAPPS/system/app/GoogleContactsSyncAdapter.apk
+rm -f vendor/evervolv/GAPPS/system/app/GoogleFeedback.apk
+rm -f vendor/evervolv/GAPPS/system/app/GooglePartnerSetup.apk
+rm -f vendor/evervolv/GAPPS/system/app/GoogleQuickSearchBox.apk
+rm -f vendor/evervolv/GAPPS/system/app/GoogleServicesFramework.apk
+rm -f vendor/evervolv/GAPPS/system/app/LatinImeTutorial.apk
+rm -f vendor/evervolv/GAPPS/system/app/Maps.apk
+rm -f vendor/evervolv/GAPPS/system/app/MarketUpdater.apk
+rm -f vendor/evervolv/GAPPS/system/app/MediaUploader.apk
+rm -f vendor/evervolv/GAPPS/system/app/Microbes.apk
+rm -f vendor/evervolv/GAPPS/system/app/NetworkLocation.apk
+rm -f vendor/evervolv/GAPPS/system/app/OneTimeInitializer.apk
+rm -f vendor/evervolv/GAPPS/system/app/SetupWizard.apk
+rm -f vendor/evervolv/GAPPS/system/app/Street.apk
+rm -f vendor/evervolv/GAPPS/system/app/Talk.apk
+rm -f vendor/evervolv/GAPPS/system/app/Twitter.apk
+rm -f vendor/evervolv/GAPPS/system/app/Vending.apk
+rm -f vendor/evervolv/GAPPS/system/app/VoiceSearch.apk
+rm -f vendor/evervolv/GAPPS/system/app/YouTube.apk
+rm -f vendor/evervolv/GAPPS/system/app/googlevoice.apk
+rm -f vendor/evervolv/GAPPS/system/app/kickback.apk
+rm -f vendor/evervolv/GAPPS/system/app/soundback.apk
+rm -f vendor/evervolv/GAPPS/system/app/talkback.apk
+rm -f vendor/evervolv/GAPPS/system/etc/permissions/android.software.sip.voip.xml
+rm -f vendor/evervolv/GAPPS/system/etc/permissions/com.google.android.maps.xml
+rm -f vendor/evervolv/GAPPS/system/etc/permissions/features.xml
+rm -f vendor/evervolv/GAPPS/system/framework/com.google.android.maps.jar
+rm -f vendor/evervolv/GAPPS/system/lib/libmicrobes_jni.so
+rm -f vendor/evervolv/GAPPS/system/lib/libspeech.so
+rm -f vendor/evervolv/GAPPS/system/lib/libvoicesearch.so
 
 mkdir -p vendor/evervolv/GAPPS/system/app
 adb pull /system/app/CarHomeGoogle.apk vendor/evervolv/GAPPS/system/app/
