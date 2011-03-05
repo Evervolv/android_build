d80a18c275f94ecc73783d0288f1a4764694319c
andrew.boren <andrew.boren@gmail.com>
2011-03-05 16:48:42 -0700

    DO NOT MERGE: 2.3.3 commits + CM
    Change-Id: I730a24d30b7de572508347d625529e9d6aeda2ff

--- tools/device/mkrecoveryzip.sh
+++ tools/device/mkrecoveryzip.sh
@@ -62,7 +62,13 @@ done
 
 
 echo 'ui_print("Replacing stock recovery with ClockworkMod recovery...");' >> $UPDATER_SCRIPT
+
+echo 'delete("sbin/recovery");' >> $UPDATER_SCRIPT
+echo 'package_extract_file("sbin/recovery", "/sbin/recovery");' >> $UPDATER_SCRIPT
+echo 'set_perm(0, 0, 0755, "/sbin/recovery");' >> $UPDATER_SCRIPT
 echo 'symlink("recovery", "/sbin/busybox");' >> $UPDATER_SCRIPT
+
+echo 'run_program("/sbin/busybox", "sh", "-c", "busybox rm -f /etc ; busybox mkdir -p /etc;");' >> $UPDATER_SCRIPT
 
 for file in $FILES
 do
@@ -86,5 +92,5 @@ rm -f $UTILITIES_DIR/update.zip
 zip -ry $UTILITIES_DIR/unsigned.zip . -x $SYMLINKS '*\[*' '*\[\[*'
 java -jar $ANDROID_ROOT/out/host/darwin-x86/framework/signapk.jar -w $ANDROID_ROOT/build/target/product/security/testkey.x509.pem $ANDROID_ROOT/build/target/product/security/testkey.pk8 $UTILITIES_DIR/unsigned.zip $UTILITIES_DIR/update.zip
 
-echo Recovery FakeFlash is now available at $OUT/recovery/update.zip
+echo Recovery FakeFlash is now available at $OUT/utilities/update.zip
 popd > /dev/null 2> /dev/null
