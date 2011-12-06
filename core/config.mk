905780341ab0bcf7f9333a4374320e61cb6718b9
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2011-12-06 19:06:56 +0059

    build/core: Include linaro_compilerchecks.mk
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

--- core/config.mk
+++ core/config.mk
@@ -124,6 +124,9 @@ TARGET_SHELL := mksh
 # are specific to the user's build configuration.
 include $(BUILD_SYSTEM)/envsetup.mk
 
+# Useful macros
+include $(BUILD_SYSTEM)/linaro_compilerchecks.mk
+
 # Boards may be defined under $(SRC_TARGET_DIR)/board/$(TARGET_DEVICE)
 # or under vendor/*/$(TARGET_DEVICE).  Search in both places, but
 # make sure only one exists.
