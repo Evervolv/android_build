3ab35a77d8ab5d5df4278571eb4fd758167f6e67
slayher <slayher@unknown.com>
2011-12-08 22:54:26 -0700

    Allow Device specific ramdisk address
    Change-Id: If0bb3d3a2b51f87a92c0b657fbc6d0277ce53532
    
    Allow OTA mkbootimg to include ramdiskaddr if defined
    
    Change-Id: Ia52ea1db06fb99d0f4831ab5045bd4e10ec3d7a8
    
    common.py: fix up compile error
    
    fix up previous commit c09b72f19d6ff7cad762226295d7d523f42bc611
    
    Change-Id: I0292e1b7c186dece87cf74ceb7f61d39bc89ced4

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1736,6 +1736,11 @@ def _BuildBootableImage(image_name, sourcedir, fs_config_file,
     cmd.append("--tags_offset")
     cmd.append(open(fn).read().rstrip("\n"))
 
+  fn = os.path.join(sourcedir, "ramdiskaddr")
+  if os.access(fn, os.F_OK):
+    cmd.append("--ramdiskaddr")
+    cmd.append(open(fn).read().rstrip("\n"))
+
   fn = os.path.join(sourcedir, "ramdisk_offset")
   if os.access(fn, os.F_OK):
     cmd.append("--ramdisk_offset")
