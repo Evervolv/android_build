51baa63a05bed706306c11f9b207950940bac3b9
Ricardo Cerqueira <github@cerqueira.org>
2011-12-11 02:15:35 -0700

    build: Preliminary support for EV product builds
    If building a ev_* product, skip crawling the filesystem for
    AndroidProduct makefiles (and, of course, parsing their contents)
    and aim directly for the device/ev/<product> device configuration
    
    Change-Id: I1d844baa80a1846a2ee636b7063a81c319d6bc17

--- envsetup.sh
+++ envsetup.sh
@@ -128,6 +128,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^ev_") ; then
+        EV_BUILD=$(echo -n $1 | sed -e 's/^ev_//g')
+    else
+        EV_BUILD=
+    fi
+    export EV_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
