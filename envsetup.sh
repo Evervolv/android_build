0ec1a5f7c53767bb50dc26c5444aaa1163c8d7e7
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-06-06 21:39:23 +0200

    env: implement the repodiff command
     sample usage:
       repodiff ics
       repodiff github/ics --numstat
       repodiff android-4.0.4_r1.2 android-4.0.4_r2
    
    Change-Id: Id1c1e504c75676707e59f051fd5ff9ce748d4297

--- envsetup.sh
+++ envsetup.sh
@@ -1703,6 +1703,15 @@ EOF
     esac
 }
 
+function repodiff() {
+    if [ -z "$*" ]; then
+        echo "Usage: repodiff <ref-from> [[ref-to] [--numstat]]"
+        return
+    fi
+    diffopts=$* repo forall -c \
+      'echo "$REPO_PATH ($REPO_REMOTE)"; git diff ${diffopts} 2>/dev/null ;'
+}
+
 # Force JAVA_HOME to point to java 1.7 if it isn't already set.
 #
 # Note that the MacOS path for java 1.7 includes a minor revision number (sigh).
