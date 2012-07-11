df6d84d7a45e795cc7b42c7c9dc90413e6f5e8d7
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-07-10 22:05:32 -0500

    build: remove OpenWnn packages
    we are going to remove these from the tree
    
    Change-Id: Id4ad6ce8e8cda70f22253da56b806909e1a49fa6

--- target/product/sdk_base.mk
+++ target/product/sdk_base.mk
@@ -29,13 +29,9 @@ PRODUCT_PACKAGES := \
 	Launcher3 \
 	LegacyCamera \
 	librs_jni \
-	libwnndict \
-	libWnnEngDic \
-	libWnnJpnDic \
 	LiveWallpapersPicker \
 	Mms \
 	Music \
-	OpenWnn \
 	Protips \
 	rild \
 	SdkSetup \
