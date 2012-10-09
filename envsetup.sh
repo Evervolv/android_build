773a6c54b8f45b55be3df497cbb372bfb3310ec9
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-10-09 18:23:37 -0500

    build: fix aosp remote
    and cleantree if $ANDROID_BUILD_TOP is null
    
    Change-Id: I1b57a481739f9988d5cd239bdba0bdc8666dae42

--- envsetup.sh
+++ envsetup.sh
@@ -1458,6 +1458,9 @@ function godir () {
 function cleantree () {
     read -p "Are you sure you want to erase local changes? (y|N)" ans
     test "$ans" = "Y" || test "$ans" = "y" || return
+    if [ ! "$ANDROID_BUILD_TOP" ]; then
+        export ANDROID_BUILD_TOP=$(gettop)
+    fi
     if [ "$(pwd)" != "$ANDROID_BUILD_TOP" ]; then
         cd "$ANDROID_BUILD_TOP"
     fi
@@ -1474,6 +1477,9 @@ function aospremote() {
     then
         echo .git directory not found. Please run this from the root directory of the Android repository you wish to set up.
     fi
+    if [ ! "$ANDROID_BUILD_TOP" ]; then
+        export ANDROID_BUILD_TOP=$(gettop)
+    fi
     PROJECT=`pwd | sed s#$ANDROID_BUILD_TOP/##g`
     if (echo $PROJECT | grep -qv "^device")
     then
@@ -1482,7 +1488,6 @@ function aospremote() {
     git remote add aosp https://android.googlesource.com/$PFX$PROJECT
     echo "Remote 'aosp' created"
 }
-export -f aospremote
 
 function evgerrit() {
     if [ $# -eq 0 ]; then
