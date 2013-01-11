e4177f1ede2d6db79d423bbcec0c233ea063a746
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-01-10 23:21:35 -0600

    envsetup: dont include device vendorsetup files
    Change-Id: I8b77be704b42a1d4d3edb7472ac4ad95a4c58a0c

--- envsetup.sh
+++ envsetup.sh
@@ -1258,9 +1258,9 @@ if [ "x$SHELL" != "x/bin/bash" ]; then
 fi
 
 # Execute the contents of any vendorsetup.sh files we can find.
-for f in `/bin/ls vendor/*/vendorsetup.sh vendor/*/*/vendorsetup.sh device/*/*/vendorsetup.sh 2> /dev/null`
+for f in `/bin/ls vendor/*/vendorsetup.sh vendor/*/*/vendorsetup.sh 2> /dev/null`
 do
-#    echo "including $f"
+    echo "including $f"
     . $f
 done
 unset f
