10e2146b3bb118566be978a16ca5e4ed315338c6
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-05-25 19:15:04 -0500

    envsetup: merge breakfast into lunch
    the lazy folk can now type "lunch mako"
    breakfast will be phased out on next release, but nightly script still
    uses it for now
    
    Change-Id: Iaef9c550d124f41dcf623671aed2e7cbe17c783d
    
    build: update our lunch additions to reflect recent changes
    
     updated to use userdebug instead of eng
    
    Change-Id: Iad20047d7142edd4cfcc78be4956d7fb0401aea7

--- envsetup.sh
+++ envsetup.sh
@@ -599,8 +599,8 @@ function lunch()
         then
             selection=${LUNCH_MENU_CHOICES[$(($answer-1))]}
         fi
-    else
-        selection=$answer
+    else #It is likely just the board name, assemble the combo for us
+        selection=ev_${answer}-userdebug
     fi
 
     export TARGET_BUILD_APPS=
