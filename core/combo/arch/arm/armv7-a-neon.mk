991935963636c2032022ec30516393c94497e894
helicopter88 <johnlemon.lol@gmail.com>
2013-08-05 14:06:45 +0100

    build: armv7-a-neon: set '-m' options for all variants
    **use -march -mtune combo for targets we build for
       (snapdragons aren't real cortex* cpus)
    **treat krait as a15
    **don't fix cortex-a8 on higher variants
    
    Signed-off-by: helicopter88 <johnlemon.lol@gmail.com>
    Signed-off-by: Andrew Sutherland <dr3wsuth3rland@gmail.com>
    Change-Id: Ic384193181d2162f533aed206f3c40fcee3dee92

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -6,19 +6,26 @@ ARCH_ARM_HAVE_VFP               := true
 ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
 
-ifeq ($(strip $(TARGET_CPU_VARIANT)), cortex-a15)
-	arch_variant_cflags := -mcpu=cortex-a15
-else
-ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a7)
+
+ifeq (,$(filter-out krait cortex-a15, $(strip $(TARGET_CPU_VARIANT))))
+	arch_variant_cflags := -march=armv7-a -mtune=cortex-a15
+else ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a9)
+	arch_variant_cflags := -march=armv7-a -mtune=cortex-a9
+else ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a8)
+	arch_variant_cflags := -march=armv7-a -mtune=cortex-a8
+else ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a7)
 	arch_variant_cflags := -mcpu=cortex-a7
 else
 	arch_variant_cflags := -march=armv7-a
-endif
 endif
 
 arch_variant_cflags += \
     -mfloat-abi=softfp \
     -mfpu=neon
 
+ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a8)
 arch_variant_ldflags := \
 	-Wl,--fix-cortex-a8
+else
+arch_variant_ldflags :=
+endif
