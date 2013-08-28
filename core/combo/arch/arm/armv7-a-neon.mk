6adcb0e1b76c899d80259b1f7367ddbe917f1158
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-08-28 18:17:04 -0500

    build: treat scorpion as cortex-a9
    but still fix cortex-a8
    * compromise between qsd8k/msm7x30 and msm8660
    
    Change-Id: I66ef59a4998a87e4c1a72443cf5e76e67dd16c5f

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -9,7 +9,7 @@ ARCH_ARM_HAVE_NEON              := true
 
 ifeq (,$(filter-out krait cortex-a15, $(strip $(TARGET_CPU_VARIANT))))
 	arch_variant_cflags := -march=armv7-a -mtune=cortex-a15
-else ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a9)
+else ifeq (,$(filter-out scorpion cortex-a9, $(strip $(TARGET_CPU_VARIANT))))
 	arch_variant_cflags := -march=armv7-a -mtune=cortex-a9
 else ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a8)
 	arch_variant_cflags := -march=armv7-a -mtune=cortex-a8
@@ -23,7 +23,7 @@ arch_variant_cflags += \
     -mfloat-abi=softfp \
     -mfpu=neon
 
-ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a8)
+ifeq (,$(filter-out scorpion cortex-a8, $(strip $(TARGET_CPU_VARIANT))))
 arch_variant_ldflags := \
 	-Wl,--fix-cortex-a8
 else
