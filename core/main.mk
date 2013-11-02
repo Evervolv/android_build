1f41892ebdbc0e9bf87f574ca923c93074120494
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-11-01 21:14:24 -0500

    allow Make 4.0
    Change-Id: Ibbe19870ee71ec7e31ae6beabdc22bdca1616f45

--- core/main.mk
+++ core/main.mk
@@ -42,12 +42,14 @@ endif
 ifeq (,$(findstring CYGWIN,$(shell uname -sm)))
 ifeq (0,$(shell expr $$(echo $(MAKE_VERSION) | sed "s/[^0-9\.].*//") = 3.81))
 ifeq (0,$(shell expr $$(echo $(MAKE_VERSION) | sed "s/[^0-9\.].*//") = 3.82))
+ifeq (0,$(shell expr $$(echo $(MAKE_VERSION) | sed "s/[^0-9\.].*//") = 4.0))
 $(warning ********************************************************************************)
 $(warning *  You are using version $(MAKE_VERSION) of make.)
 $(warning *  Android is tested to build with versions 3.81 and 3.82.)
 $(warning *  see https://source.android.com/source/download.html)
 $(warning ********************************************************************************)
 $(error stopping)
+endif
 endif
 endif
 endif
