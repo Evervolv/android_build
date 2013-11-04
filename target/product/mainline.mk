6bd08ef5b8fef50401f98b3710e0b1379daf889d
Jiangyi <sam.andrew.jiang@gmail.com>
2013-11-04 10:41:57 -0500

    Don't copy APN list from emulator
    This list is severely incomplete and is overwriting the proper APN list in the vendor
    directory.
    
    Change-Id: I0e1b1d26e0cdf2df9a05462f0c9343bd3115635b
    
    Do not use the default APN
    
    Device should provide it's own apns-conf.xml
    
    Change-Id: Ib245e72d5ffe1b874e6db031d0afe2d7c97050f1

--- target/product/mainline.mk
+++ target/product/mainline.mk
@@ -33,5 +33,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     PhotoTable \
     WallpaperPicker \
-
-PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
