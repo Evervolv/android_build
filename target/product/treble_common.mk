5ab2a3973c0f8616171bfaef3363122485b2d10f
Jiangyi <sam.andrew.jiang@gmail.com>
2013-11-04 10:41:57 -0500

    Don't copy APN list from emulator
    This list is severely incomplete and is overwriting the proper APN list in the vendor
    directory.
    
    Change-Id: I0e1b1d26e0cdf2df9a05462f0c9343bd3115635b

--- target/product/treble_common.mk
+++ target/product/treble_common.mk
@@ -40,11 +40,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
 
-# Telephony:
-#   Provide a default APN configuration
-PRODUCT_COPY_FILES += \
-    device/generic/goldfish/data/etc/apns-conf.xml:system/etc/apns-conf.xml
-
 # NFC:
 #   Provide default libnfc-nci.conf file for devices that does not have one in
 #   vendor/etc
