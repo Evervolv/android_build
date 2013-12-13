23260e98ef3f4bb885372e8d98a81251e27ec6bd
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-12-12 21:41:04 -0600

    use cm logic to find products
    Change-Id: Ib0abdc81a66d125f30ed919855f8779a6ff75018

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,7 +185,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
   ifneq ($(EV_BUILD),)
-    $(call import-products, device/*/$(EV_BUILD)/ev.mk)
+    all_product_configs := $(shell ls device/*/$(EV_BUILD)/ev.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
@@ -193,6 +193,7 @@ else
   endif
 endif
 
+ifeq ($(EV_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
@@ -214,6 +215,10 @@ $(foreach f, $(all_product_configs),\
 _cpm_words :=
 _cpm_word1 :=
 _cpm_word2 :=
+else
+    current_product_makefile := $(strip $(all_product_configs))
+    all_product_makefiles := $(strip $(all_product_configs))
+endif
 current_product_makefile := $(strip $(current_product_makefile))
 all_product_makefiles := $(strip $(all_product_makefiles))
 
