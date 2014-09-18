657eca8694d5f97bbe8fe87d6a29ae27be751fa6
Tom Marshall <tdm@cyngn.com>
2014-09-18 09:45:33 -0700

    build: qcom: Set QCOM_*_PATH variables for hardware/qcom-* projects
    This consolidates a bunch of one-off logic scattered throughout the
    code base.  Usage in Android.mk files is trivial:
    
    At top level, use e.g. "ifeq ($(call my-dir),$(QCOM_AUDIO_PATH))".
    This works for all variants, including non-variants (i.e. AOSP).
    
    Within subdirs, use e.g. hardware/qcom/audio => $(QCOM_AUDIO_PATH)
    
    Change-Id: Iee2497ea9a7efeb4ae9e861b84c532b19da7b69d
    
    build: Introduce project pathmap and use it for qcom variants
    
    The project pathmap tracks the path to the top level of a project.  The
    following functions are provided:
    
     * project-set-path adds an entry.
     * project-set-path-variant adds a "variant" entry.
     * project-path-for retrieves an entry.
    
    To use as a guard in Android.mk:
    
      ifeq ($(call my-dir),$(call project-path-for,projectname))
    
    To use for include paths in Android.mk:
    
      LOCAL_C_INCLUDES += $(call project-path-for,projectname)/...
    
    Set project pathmap for qcom project variants.
    
    Change-Id: I8dceca72a1ba80fc7b1830c5ab285d444f530457
    
    build: Set QCOM variants for non-QCOM_HARDWARE defined targets
    
    * Nexus devices and others typically do not define the QCOM_HARDWARE
      flag, so the variant path should always default to the AOSP variant.
    * Unconditionally set the variant to the AOSP HAL by default.
    
    Change-Id: I714170897128f92718af266366cfcbf3136e8981

--- core/config.mk
+++ core/config.mk
@@ -706,6 +706,9 @@ endif
 FRAMEWORK_MANIFEST_FILE := system/libhidl/manifest.xml
 FRAMEWORK_COMPATIBILITY_MATRIX_FILE := hardware/interfaces/compatibility_matrix.xml
 
+# Rules for QCOM targets
+include $(SRC_EVERVOLV_DIR)/build/core/qcom_target.mk
+
 # ###############################################################
 # Set up final options.
 # ###############################################################
