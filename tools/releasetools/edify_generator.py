d9aeaa32f525885f9e3a9baa9c97a2e466ead42b
Tom Marshall <tdm@cyngn.com>
2014-12-12 11:51:33 -0800

    build: ota: Support for install tools in /tmp/install
     * Anything in OUT/install gets packaged up into the zip and extracted
       to /tmp/install immediately after FullOTA_InstallBegin.
    
     * Use /tmp/install in edify scripts and remove code related to using
       and manipulating /system for install tools.
    
    Change-Id: I315a3238e36c8d15e26f935e272f7e27dd59c320

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -149,18 +149,7 @@ class EdifyGenerator(object):
     self.script.append(self.WordWrap(cmd))
 
   def RunBackup(self, command):
-    self.script.append('package_extract_file("system/bin/backuptool.sh", "/tmp/backuptool.sh");')
-    self.script.append('package_extract_file("system/bin/backuptool.functions", "/tmp/backuptool.functions");')
-    if not self.info.get("use_set_metadata", False):
-      self.script.append('set_perm(0, 0, 0755, "/tmp/backuptool.sh");')
-      self.script.append('set_perm(0, 0, 0644, "/tmp/backuptool.functions");')
-    else:
-      self.script.append('set_metadata("/tmp/backuptool.sh", "uid", 0, "gid", 0, "mode", 0755);')
-      self.script.append('set_metadata("/tmp/backuptool.functions", "uid", 0, "gid", 0, "mode", 0644);')
-    self.script.append(('run_program("/tmp/backuptool.sh", "%s");' % command))
-    if command == "restore":
-        self.script.append('delete("/system/bin/backuptool.sh");')
-        self.script.append('delete("/system/bin/backuptool.functions");')
+    self.script.append(('run_program("/tmp/install/bin/backuptool.sh", "%s");' % command))
 
   def ShowProgress(self, frac, dur):
     """Update the progress bar, advancing it over 'frac' over the next
