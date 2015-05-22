e28bec878cffda2c0e2920bae2a518a51ecf87da
Howard M. Harte <hharte@cyngn.com>
2015-05-22 09:54:20 -0700

    minnow_max: search for ev.mk in additional directories
    Intel's product configuration files are structured slightly
    differentily than others.  Device-specific configuration is nested
    one additional level, as follows:
    
    device/intel/<family>/<product>/
    
    Change-Id: I78b02978dc759b94024e5c5533d1108ac2634549

--- core/product_config.mk
+++ core/product_config.mk
@@ -180,7 +180,7 @@ include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
 ifneq ($(EV_BUILD),)
-  all_product_configs := $(shell ls device/*/$(EV_BUILD)/ev.mk)
+  all_product_configs := $(shell find device -path "*/$(EV_BUILD)/ev.mk")
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
     # An unbundled app build needs only the core product makefiles.
