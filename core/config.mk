28f48cfe21f59353b95ca204f125eb9482d4fecb
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-08-26 00:37:02 +0300

    Source EV specific envsetup
    Change-Id: I39d3e711a6beab47e3284919a9a4fef15b0f40b9
    
    build: Ensure that we can always find the CM envsetup script
    
    Change-Id: Ie145a2667cd14f7e4e4622a0d8b46f1e01c0b3c3
    
    build: add ev target dir
    
    Change-Id: Iec754a72d391722c580916b12480ccdc3e423c2d
    
    build: import changes from ev config.mk
    
     That will handle qcom_targets.mk inclusion
    
    Change-Id: I987ad65becc568ea54c5fb6dea5957f96dcd7511
    
    build: Include Evervolv specific properties in build.prop
    
     * Our properties were supposed to go to /system/etc/prop.default
        after the following commit:
        "lineage: Move to Google's method of defining system default props"
        Change-Id: I6cb0e28a7599b010b389cc541015a37010a00f4b
    
     * However if BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED is not true,
        only /default.prop will retain the properties contents of
        ADDITIONAL_DEFAULT_PROPERTIES and PRODUCT_SYSTEM_DEFAULT_PROPERTIES,
        and none of our versioning identification was held in the system
    
     * Enabling BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED globally would
        break all properties on devices that handle partitions usually
        at the device level rather than the kernel due to mounting races
    
     * Include the vendor/lineage/config/main.mk entrypoint to be allowed
        to extend ADDITION_BUILD_PROPERTIES as we need to define our releases
    
    Change-Id: I19918eece0f0dd3ee967db0339b4d09c9a6d540e
    Signed-off-by: Adrian DC <radian.dc@gmail.com>
    
    ev: selinux policy inclusion
    
    We're adding CM-specific policies to an overlay instead of patching
    external/sepolicy, so we need to make sure it's included to prevent
    misbehaving systems
    
    Change-Id: I7989a1093f9abc8360d492e73a860eb4afad10ad
    
    build: Source a CM-specific global BoardConfig
    
     * Read vendor/cm/config/BoardConfigCM.mk. Kind of surprised we haven't
       needed this until now.
    
    Change-Id: I91cd89b14f6a5975e91f15dcfce1070502aa3861
    
    build: qcom: Set QCOM_*_PATH variables for hardware/qcom-* projects
    
    This consolidates a bunch of one-off logic scattered throughout the
    code base.  Usage in Android.mk files is trivial:
    
    At top level, use e.g. "ifeq ($(call my-dir),$(QCOM_AUDIO_PATH))".
    This works for all variants, including non-variants (i.e. AOSP).
    
    Within subdirs, use e.g. hardware/qcom/audio => $(QCOM_AUDIO_PATH)
    
    Change-Id: Iee2497ea9a7efeb4ae9e861b84c532b19da7b69d
    
    build: Introduce project pathmap and use it for qcom variants
    
    The project pathmap tracks the path to the top level of a project.  The
    following functions are provided:
    
     * project-set-path adds an entry.
     * project-set-path-variant adds a "variant" entry.
     * project-path-for retrieves an entry.
    
    To use as a guard in Android.mk:
    
      ifeq ($(call my-dir),$(call project-path-for,projectname))
    
    To use for include paths in Android.mk:
    
      LOCAL_C_INCLUDES += $(call project-path-for,projectname)/...
    
    Set project pathmap for qcom project variants.
    
    Change-Id: I8dceca72a1ba80fc7b1830c5ab285d444f530457
    
    build: Set QCOM variants for non-QCOM_HARDWARE defined targets
    
    * Nexus devices and others typically do not define the QCOM_HARDWARE
      flag, so the variant path should always default to the AOSP variant.
    * Unconditionally set the variant to the AOSP HAL by default.
    
    Change-Id: I714170897128f92718af266366cfcbf3136e8981
    
    Add wildcards to allow lineage gsi targets
    
    Change-Id: I637ddf7df14883c75f42ae17edc815dbadcf7fde

--- core/config.mk
+++ core/config.mk
@@ -185,6 +185,9 @@ UNAME := $(shell uname -sm)
 
 SRC_TARGET_DIR := $(TOPDIR)build/make/target
 
+# Evervolv
+SRC_EVERVOLV_DIR := $(TOPDIR)vendor/ev
+
 # Some specific paths to tools
 SRC_DROIDDOC_DIR := $(TOPDIR)build/make/tools/droiddoc
 
@@ -452,6 +455,8 @@ endif
 # Pruned directory options used when using findleaves.py
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
+
+include $(SRC_EVERVOLV_DIR)/build/target/board/BoardConfigCommon.mk
 
 # The build system exposes several variables for where to find the kernel
 # headers:
@@ -1089,6 +1094,9 @@ endif # PRODUCT_USE_DYNAMIC_PARTITIONS
 BOARD_PREBUILT_HIDDENAPI_DIR ?=
 .KATI_READONLY := BOARD_PREBUILT_HIDDENAPI_DIR
 
+# Rules for Evervolv targets
+include $(SRC_EVERVOLV_DIR)/build/core/config.mk
+
 # ###############################################################
 # Set up final options.
 # ###############################################################
@@ -1284,6 +1292,12 @@ include $(BUILD_SYSTEM)/sysprop_config.mk
 # Make ANDROID Soong config variables visible to Android.mk files, for
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
+
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+ifneq (,$(wildcard device/evervolv/sepolicy/common/sepolicy.mk))
+$(eval include device/evervolv/sepolicy/common/sepolicy.mk)
+endif
 
 # EMMA_INSTRUMENT is set to true when coverage is enabled. Creates a suffix to
 # differeciate the coverage version of ninja files. This will save 5 minutes of
