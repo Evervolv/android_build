4d80794ff6966e8cd1046c71355a11702b25624c
Chirayu Desai <chirayudesai1@gmail.com>
2016-08-26 22:13:51 +0530

    envsetup: fix lunch
    Change-Id: I9dea4605857741132b8d10143c20d4bd7829086a

--- envsetup.sh
+++ envsetup.sh
@@ -623,10 +623,7 @@ function lunch()
         return 1
     fi
 
-    TARGET_PRODUCT=$product \
-    TARGET_BUILD_VARIANT=$variant \
-    TARGET_PLATFORM_VERSION=$version \
-    build_build_var_cache
+    check_product $product
     if [ $? -ne 0 ]
     then
         # if we can't find a product, try to grab it off the Evervolv github
@@ -639,6 +636,10 @@ function lunch()
         vendor/ev/build/tools/roomservice.py $product true
     fi
 
+    TARGET_PRODUCT=$product \
+    TARGET_BUILD_VARIANT=$variant \
+    TARGET_PLATFORM_VERSION=$version \
+    build_build_var_cache
     if [ $? -ne 0 ]
     then
         echo
