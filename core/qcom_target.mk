de56a4add646e273fd9166803e2591692eccbcf6
Nick Reuter <nreuter85@gmail.com>
2016-08-28 00:01:46 -0500

    Revert "core: qcom_target: clean out cflags"
    This reverts commit f66f2cf209b85b1234af47f8096ab1f0a66d2d7a.

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -23,12 +23,38 @@ endef
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
+    qcom_flags := -DQCOM_HARDWARE
     ifneq ($(BOARD_USES_LEGACY_QCOM_DISPLAY),true)
+        qcom_flags += -DQCOM_BSP
+        qcom_flags += -DQTI_BSP
+
         TARGET_USES_QCOM_BSP := true
     endif
 
     # Tell HALs that we're compiling an AOSP build with an in-line kernel
     TARGET_COMPILE_WITH_MSM_KERNEL := true
+
+    ifneq ($(filter msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),)
+        ifneq ($(BOARD_USES_LEGACY_QCOM_DISPLAY),true)
+            # Enable legacy graphics functions
+            qcom_flags += -DQCOM_BSP_LEGACY
+        endif
+        # Enable legacy audio functions
+        ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
+            qcom_flags += -DLEGACY_ALSA_AUDIO
+        endif
+    endif
+
+    TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+    CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+
+    # Multiarch needs these too..
+    2ND_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    2ND_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+    2ND_CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    2ND_CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
 
     ifeq ($(QCOM_HARDWARE_VARIANT),)
         ifneq ($(filter msm8610 msm8226 msm8974,$(TARGET_BOARD_PLATFORM)),)
