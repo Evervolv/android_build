8cdc6ff4a6a0e883519e102a3070d3fdee89e656
beanstown106 <nbenis106@gmail.com>
2016-12-25 13:40:14 -0500

    GoogleAudio: set new pixel sounds as defaults
    Change-Id: Id9806d641adf22b741d9d4bc09b1c5e3ebd2bf2a

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -24,15 +24,19 @@ PRODUCT_PACKAGES := \
     WAPPushManager
 
 # Additional settings used in all AOSP builds
+ifeq ($(wildcard vendor/google/GoogleAudio.mk),)
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg
+endif
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
 # Get some sounds
+ifeq ($(wildcard vendor/google/GoogleAudio.mk),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+endif
 
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
