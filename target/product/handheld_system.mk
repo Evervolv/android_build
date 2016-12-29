adf9c5266a583b313a5541ee51202ea8b43bc5b1
David Kataja Jr <krash86.7@gmail.com>
2016-12-29 10:48:04 -0500

    Allow device tree to override audio_effects.conf
    Change-Id: I858df647375d8bbf689189c99e4d01c37e46062e

--- target/product/handheld_system.mk
+++ target/product/handheld_system.mk
@@ -80,7 +80,7 @@ PRODUCT_SYSTEM_SERVER_APPS += \
     KeyChain \
     Telecom \
 
-PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES ?= \
     frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf
 
 PRODUCT_VENDOR_PROPERTIES += \
