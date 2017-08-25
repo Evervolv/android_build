ce8b49ccd25f8fb334a5e5ef5d4ce376a3074db9
George G <kreach3r@users.noreply.github.com>
2017-08-25 09:23:37 +0200

    Disable the after flash image verification
    This verification produced failed installations on MultiROM. Get rid of it until further notice.
    
    Change-Id: Iaf8df61fe2eae686b1495da3544ea70887799552

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1459,8 +1459,6 @@ class BlockDifference(object):
     if progress:
       script.ShowProgress(progress, 0)
     self._WriteUpdate(script, output_zip)
-    if OPTIONS.verify:
-      self._WritePostInstallVerifyScript(script)
 
   def WriteStrictVerifyScript(self, script):
     """Verify all the blocks in the care_map, including clobbered blocks.
@@ -1636,8 +1634,7 @@ class BlockDifference(object):
 
     call = ('block_image_update("{device}", '
             'package_extract_file("{partition}.transfer.list"), '
-            '"{new_data_name}", "{partition}.patch.dat") ||\n'
-            '  abort("E{code}: Failed to update {partition} image.");'.format(
+            '"{new_data_name}", "{partition}.patch.dat");'.format(
                 device=self.device, partition=self.partition,
                 new_data_name=new_data_name, code=code))
     script.AppendExtra(script.WordWrap(call))
