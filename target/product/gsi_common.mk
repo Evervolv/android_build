ced852b89d21771f9f51144081addfce2154522e
Michael Bestas <mkbestas@lineageos.org>
2019-04-12 02:10:12 +0300

    build: Include AudioPackage14.mk instead of AllAudio.mk
    * AllAudio.mk doesn't contain latest material files and also
      contains multiple duplicate files. Just copy the latest
      material ones and we'll include the older files in vendor/lineage
    
    Change-Id: Ic7ae8f63d4f86cd39f86aeea719bfb8e065bf800

--- target/product/gsi_common.mk
+++ target/product/gsi_common.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
 # Default AOSP sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
 
 # GSI doesn't support apex for now.
 # Properties set in product take precedence over those in vendor.
--- target/product/mainline.mk
+++ target/product/mainline.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
-$(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product, frameworks/base/data/sounds/AudioPackage14.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
