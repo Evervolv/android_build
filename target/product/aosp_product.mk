1e29529ef8a9ea316145cd8e4499d54864b89e72
Michael Bestas <mkbestas@lineageos.org>
2019-04-12 02:10:12 +0300

    build: Include AudioPackage14.mk instead of AllAudio.mk
    * AllAudio.mk doesn't contain latest material files and also
      contains multiple duplicate files. Just copy the latest
      material ones and we'll include the older files in vendor/lineage
    
    Change-Id: Ic7ae8f63d4f86cd39f86aeea719bfb8e065bf800

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
 # Default AOSP sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
 
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
