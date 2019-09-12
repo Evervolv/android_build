86d5a8f015ded46c1a721f5c3a5dfc2e86d6930b
Jan Altensen <info@stricted.net>
2019-09-12 21:59:18 +0200

    build: set fingerprint in ro.build.fingerprint
    [kubersharma001]:
    * Adapt to android-12.0:
    
    Change-Id: Iffc6583aa624bd74f420ce060f0e612769f81735

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -54,6 +54,9 @@ echo "ro.build.product=$TARGET_DEVICE"
 
 echo "# Do not try to parse description or thumbprint"
 echo "ro.build.description=$PRIVATE_BUILD_DESC"
+
+echo "# Do not try to parse fingerprint or thumbprint"
+echo "ro.build.fingerprint=$BUILD_FINGERPRINT"
 if [ -n "$BUILD_THUMBPRINT" ] ; then
   echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
 fi
